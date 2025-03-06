8d88396538ff40d04f8c2c811faf5d6c8ba46663
Yusuf Babatunde <Yusuf.itexpert@gmail.com>
2025-03-06 06:06:33 -0800

    cart length fix

--- pizza-app/src/App.jsx
+++ pizza-app/src/App.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import AppBar from "./components/appbar";
 import Order from "./components/order";
 import PizzaOfTheDay from "./components/pizza-of-the-day";
+import { CartContext } from "./components/contexts";
 
 function App() {
+  const cartHook = useState([]);
   return (
-      <StrictMode>
+    <StrictMode>
+      <CartContext.Provider value={cartHook}>
         <AppBar />
-      <Order />
-     <PizzaOfTheDay/>
-      </StrictMode>
+        <Order />
+        <PizzaOfTheDay />
+      </CartContext.Provider>
+    </StrictMode>
   );
 }
 
